eb2c002ac6f60fd55f0c39460aaa1a143b6b00d0
Monali-2105git config --global user.nemail nikammonali55@gmail.comgit config --global user.name Monali-2105 <nikammonali55@gnail.com>
2026-06-27 17:44:05 +0530

    fixed cors

--- backend/app.js
+++ backend/app.js
@@ -8,19 +8,20 @@ import cookieParser from "cookie-parser";
 import fileupload from "express-fileupload";
 
 const app = express();
-app.use(cors());
-app.get("/test", (req, res) => {
-  res.json({ message: "CORS is working" });
-});
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173", // Local Vite frontend
-//       "agri-market-20rw8qupm-monali-2105s-projects.vercel.app" // Replace with your Vercel URL
-//     ],
-//     credentials: true,
-//   })
-// );
+// app.use(cors());
+// app.get("/test", (req, res) => {
+//   res.json({ message: "CORS is working" });
+// });
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Local Vite frontend
+      "https://agri-market-20rw8qupm-monali-2105s-projects.vercel.app", // Replace with your Vercel URL
+      "https://agri-market-eosin.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 // INCREASE THE LIMITS HERE (50mb is usually plenty for images)
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
